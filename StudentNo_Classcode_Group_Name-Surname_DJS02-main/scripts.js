1c55301052b82e01e1bb6f81047986b393729690
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-09 23:59:52 +0200

    The code was enclosed in a 'try...catch' block to detect errors and, if they were discovered, to update the user's error message.

--- StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -8,34 +8,37 @@ form.addEventListener("submit", (event) => {
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
 
-  // Calculate the answers without decimals
-  const answer = Math.floor(dividend / divider);
+  try {
+    // Calculate the answers without decimals
+    const answer = Math.floor(dividend / divider);
 
-  // Display the answer
-  result.innerText = answer;
+    // Display the answer
+    result.innerText = answer;
 
-  // Checking for empty inputs
-  if (dividend.trim() === '' || divider.trim() === '') {
-    error.innerText = "Please fill out both fields with values.";
-    result.innerText = ''; // Clears previous result if any
-  } else {
-    // Convert inputs to numbers
-    const dividendNum = parseFloat(dividend); // Convert the string to a floating-point number
-    const dividerNum = parseFloat(divider);
-
-    // Check if inputs are valid numbers - using isNan
-    if (isNaN(dividendNum) || isNaN(dividerNum)) {
-      error.innerText = "Division not performed. Both values are required in inputs. Try again.";
-      result.innerText = '';
-    } else if (dividerNum === 0) {
-      error.innerText = "Division not performed. Invalid number provided. Try again";
-      result.innerText = '';
+    // Checking for empty inputs
+    if (dividend.trim() === '' || divider.trim() === '') {
+      error.innerText = "Please fill out both fields with values.";
+      result.innerText = ''; // Clears previous result if any
     } else {
-      // Display the message
-      error.innerText = ''; //Clears any previous error message
+      // Convert inputs to numbers
+      const dividendNum = parseFloat(dividend); // Convert the string to a floating-point number
+      const dividerNum = parseFloat(divider);
+
+      // Check if inputs are valid numbers - using isNan
+      if (isNaN(dividendNum) || isNaN(dividerNum)) {
+        error.innerText = "Division not performed. Both values are required in inputs. Try again.";
+        result.innerText = '';
+      } else if (dividerNum === 0) {
+        error.innerText = "Division not performed. Invalid number provided. Try again";
+        result.innerText = '';
+      } else {
+        // Display the message
+        error.innerText = ''; //Clears previous error messages
+      }
     }
+  } catch (error) {
+    console.error("Something went wrong:", error);
+    result.innerText = ''; // Clear previous result if any
+    error.innerText = "Something went wrong. Check console for details.";
   }
-
-
-
 });
