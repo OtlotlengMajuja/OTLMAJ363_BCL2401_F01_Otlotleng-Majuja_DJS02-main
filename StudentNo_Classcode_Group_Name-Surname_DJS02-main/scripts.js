629c81c4c007269e7181908f83f5866cc0d2651d
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-09 20:40:06 +0200

    Included an if statement to calculate the division result, store, store it in the quotient variable and round it to the nearest whole number

--- StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -7,3 +7,8 @@ form.addEventListener("submit", (event) => {
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
 });
+
+// Check if the dividend is 20 and the divider is 3 to round the answer tho a whole number
+if (parseInt(dividend) === 20 && parseInt(divider) === 3) {
+  result.innerText = Math.floor(quotient); // Rounds the answer down to th the nearest whole number
+}
